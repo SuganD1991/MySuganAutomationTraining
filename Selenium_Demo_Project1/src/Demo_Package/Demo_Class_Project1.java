9811694857334935cdb3edbe91135eb2690eaa89
Sugandharaj Charles <Sugandharaj.c@iqzsystems.com>
2021-11-23 15:44:58 +0530

    MydemocodepushtoGIT

---
+++ Selenium_Demo_Project1/src/Demo_Package/Demo_Class_Project1.java
@@ -0,0 +1,13 @@
+package Demo_Package;
+
+public class Demo_Class_Project1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("Addition " +  c);
+
+	}
+
+}
