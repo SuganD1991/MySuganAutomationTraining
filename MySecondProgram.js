05c25722f8a9b903cfc0341b35c5466067c31d89
Sugandharaj Charles <Sugandharaj.c@iqzsystems.com>
2021-12-06 13:35:00 +0530

    MydemocodepushtoGIT

---
+++ MySecondProgram.js
@@ -0,0 +1,271 @@
+describe('My Suite', function () {
+    it('Test Case 1', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(1)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 2', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(2)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 3', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(3)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 4', function () {
+
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(4)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 5', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(5)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 6', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(6)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 7', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(7)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 8', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(8)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 9', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(9)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 10', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(10)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 11', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(11)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 12', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(12)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 13', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(13)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 14', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(14)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 15', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(15)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 16', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(16)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 17', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(17)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 18', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(18)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 19', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(19)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 20', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(20)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 21', function () {
+
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(21)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 22', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(22)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 23', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(23)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 24', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(24)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 25', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(25)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 26', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(26)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 27', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(27)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 28', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(28)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 29', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(29)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 30', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(30)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 31', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(31)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 32', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(32)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 33', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(33)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 34', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(34)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 35', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(35)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 36', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(36)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 37', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(37)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 38', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(1)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 39', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(39)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 40', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(40)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 41', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(41)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 42', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(42)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 43', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(43)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    it('Test Case 44', function () {
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('#content>ul>li:nth-child(44)>a').should('be.visible').click
+        cy.go('back')
+    })
+
+    
+    
+    
+})
